bf9d349d27372678f47cd33aa7869825041eb57c
dpopesco <diana.popesco17@gmail.com>
2022-11-01 19:55:13 +0200

    Initial commit

---
+++ src/test/java/TC01GetRequest.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC01GetRequest {
+    @Test
+    void getWeatherDetails() {
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+
+        System.out.println(response.asString());
+        System.out.println(response.getBody().asString());
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getStatusLine());
+        System.out.println(response.getHeader("content-type"));
+        System.out.println(response.getTime());
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+}
